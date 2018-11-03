8b7a2de7ae05417fffdd74560dd8e0dd2b0ca62d
Colin Misbach <colin@misbach.com>
2018-11-02 22:26:09 -0600

    IP address fills in at start

--- Assets/Scripts/UI/Menu.cs
+++ Assets/Scripts/UI/Menu.cs
@@ -9,6 +9,11 @@ public class Menu : MonoBehaviour
     [SerializeField] NetworkManager m_networkManager = null;
     [SerializeField] TMP_InputField m_ipAddress = null;
 
+    private void Start()
+    {
+        m_ipAddress.text = m_networkManager.networkAddress;
+    }
+
     public void StartServer()
     {
         m_networkManager.StartServer();
