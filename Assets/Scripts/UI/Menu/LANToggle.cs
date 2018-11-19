9ba2accdb8999aa1ecc1869503372bd4002cb178
Colin Misbach <colin@misbach.com>
2018-11-18 23:21:01 -0700

    Pretty Menu

--- Assets/Scripts/UI/Menu/LANToggle.cs
+++ Assets/Scripts/UI/Menu/LANToggle.cs
@@ -9,18 +9,29 @@ public class MenuToggle : UnityEvent<bool> { }
 public class LANToggle : MonoBehaviour
 {
     [SerializeField] MenuToggle m_lanModeToggle = null;
+    [SerializeField] MenuOptions m_menuOptions = null;
+    [SerializeField] [Range(0.0f, 3.0f)] float m_cooldown = 0.3f;
 
     public bool LAN { get { return m_enabled; } }
 
     bool m_enabled;
+    float m_time;
+    UIButton m_button;
 
     private void Start()
     {
+        m_button = GetComponent<UIButton>();
         m_enabled = false;
+        m_time = -m_cooldown;
     }
 
     public void Toggle()
     {
+        float delta = Time.time - m_time;
+        if (delta < m_cooldown)
+            return;
+        m_time = Time.time;
+
         m_enabled = !m_enabled;
 
         if (m_enabled)
@@ -31,6 +42,9 @@ public class LANToggle : MonoBehaviour
         {
             DisableLAN();
         }
+
+        m_menuOptions.ToggleSize();
+        m_button.Swell();
     }
 
     void EnableLAN()
