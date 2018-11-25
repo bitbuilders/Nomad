32350570f98c1a67f7f36a554dd6df1e33fb09b7
Colin Misbach <colin@misbach.com>
2018-11-25 16:10:10 -0700

    Refresh LAN game list button and automatic

--- Assets/Scripts/UI/Menu/PlayerGameManager.cs
+++ Assets/Scripts/UI/Menu/PlayerGameManager.cs
@@ -7,12 +7,25 @@ public class PlayerGameManager : Singleton<PlayerGameManager>
     [SerializeField] GameObject m_playerGameTemplate = null;
     [SerializeField] Transform m_playerGameLocation = null;
     [SerializeField] GameObject m_playerGameView = null;
+    [SerializeField] [Range(0.0f, 30.0f)] float m_automaticRefreshRate = 10.0f;
 
     List<PlayerGame> m_playerGames;
+    float m_refreshTime;
 
     private void Start()
     {
         m_playerGames = new List<PlayerGame>();
+        m_refreshTime = 0.0f;
+    }
+
+    private void Update()
+    {
+        m_refreshTime += Time.deltaTime;
+        if (m_refreshTime >= m_automaticRefreshRate)
+        {
+            m_refreshTime = 0.0f;
+            RefreshList();
+        }
     }
 
     public void CreatePlayerGame(string ip, string hostName)
