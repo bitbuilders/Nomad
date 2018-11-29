a50bc0c5faad070bce5bb5d51076743ec3585dc5
Colin Misbach <colin@misbach.com>
2018-11-29 00:22:36 -0700

    started billboard | Probuilder

---
+++ Assets/Scripts/Environment/Billboard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    [SerializeField] BillBoardGame m_billboardGame = null;
+    [SerializeField] [Range(0.0f, 10.0f)] float m_interactionDistance = 3.0f;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("PlayBillboard"))
+        {
+            PlayBillboard();
+        }
+    }
+
+    public void PlayBillboard()
+    {
+
+    }
+}
